bd2e3c8968d65ec6384d162f052ee4fafd360a02
Oliver Marriott <hello@omarriott.com>
2021-06-02 03:48:58 +1000

    gen users

--- lib/bug_web.ex
+++ lib/bug_web.ex
@@ -83,6 +83,7 @@ defmodule BugWeb do
 
       # Import LiveView helpers (live_render, live_component, live_patch, etc)
       import Phoenix.LiveView.Helpers
+      import BugWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
---
+++ lib/bug_web/live/live_helpers.ex
@@ -0,0 +1,23 @@
+defmodule BugWeb.LiveHelpers do
+  import Phoenix.LiveView.Helpers
+
+  @doc """
+  Renders a component inside the `BugWeb.ModalComponent` component.
+
+  The rendered modal receives a `:return_to` option to properly update
+  the URL when the modal is closed.
+
+  ## Examples
+
+      <%= live_modal @socket, BugWeb.UserLive.FormComponent,
+        id: @user.id || :new,
+        action: @live_action,
+        user: @user,
+        return_to: Routes.user_index_path(@socket, :index) %>
+  """
+  def live_modal(socket, component, opts) do
+    path = Keyword.fetch!(opts, :return_to)
+    modal_opts = [id: :modal, return_to: path, component: component, opts: opts]
+    live_component(socket, BugWeb.ModalComponent, modal_opts)
+  end
+end
